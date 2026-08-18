9f64630ac4e82ffabd041d1cdd8b7b9e85c58f7b
RIFADEV <official.rifanesia@gmail.com>
2026-08-18 22:27:33 +0900

    fix: STRINTERNALNAME use build timestamp instead of git hash

--- scripts/gen_version.bat
+++ scripts/gen_version.bat
@@ -9,10 +9,12 @@ for /f %%i in ('git -C "%GITROOT%" describe --tags --always --dirty 2^>nul') do 
 if not defined HASH set "HASH=nogit"
 if not defined REV set "REV=0"
 if not defined TAG set "TAG=v0"
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value 2^>nul') do set "dt=%%a"
+set "TIMESTAMP=%dt:~0,4%/%dt:~4,2%/%dt:~6,2% %dt:~8,2%:%dt:~10,2%:%dt:~12,2%"
 >  "%OUT%" echo #pragma once
 >> "%OUT%" echo #define FILEVER        %REV%
 >> "%OUT%" echo #define STRFILEVER     "%REV%"
->> "%OUT%" echo #define STRINTERNALNAME "%HASH%"
+>> "%OUT%" echo #define STRINTERNALNAME "%TIMESTAMP%"
 >> "%OUT%" echo #define GIT_REV  "%REV%"
 >> "%OUT%" echo #define GIT_HASH "%HASH%"
 >> "%OUT%" echo #define GIT_TAG  "%TAG%"
